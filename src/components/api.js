080a64734d943d03d3856d275a426f37f2a276df
Azat <azat1hajy@gmail.com>
2023-02-12 21:58:44 +0300

    forecast

--- src/components/api.js
+++ src/components/api.js
@@ -9,7 +9,8 @@ export const APIOptions = {
 export const GET_API='https://wft-geo-db.p.rapidapi.com/v1/geo/cities?minPopulation=100000';
 
 export const OPEN_WEATHER_KEY='814a79c17d4df4e9e8b2bcacf2722dc8';
-export const GET_OPENWEATHER='https://api.openweathermap.org/data/2.5/weather?'
+export const GET_OPENWEATHER_DAILY='https://api.openweathermap.org/data/2.5/weather?'
 //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-
+export const GET_OPENWEATHER_FORECAST = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
 //https://api.openweathermap.org/data/2.5/weather?&appid=814a79c17d4df4e9e8b2bcacf2722dc8
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
