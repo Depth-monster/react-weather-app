4e9064d8742ec97431f80c75ff103fb0d0395aac
Azat <azat1hajy@gmail.com>
2023-02-13 19:57:48 +0300

    successfull media

--- src/components/forecast/Forecast.js
+++ src/components/forecast/Forecast.js
@@ -3,17 +3,13 @@ import "./Forecast.css";
 
 const Forecast = ({ forecast }) => {
   console.log(forecast);
-  const screenWidth = window.innerWidth;
-  let itemWidth = 300;
   
-  if (screenWidth < 768) {
-    itemWidth = screenWidth - 40;
-  }
+
   return (
     <div className="forecast-main">
       <div className="forecast-list">
         {forecast.list.map((id) => (
-          <div className="forecast-item" style={{ width: itemWidth }}>
+          <div className="forecast-item">
 
             <p>{`${id.dt_txt}`}</p>
           </div>
